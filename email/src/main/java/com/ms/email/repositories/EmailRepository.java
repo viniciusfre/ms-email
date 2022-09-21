0239cfc6cbe3857a8cd7dc168820508708762058
Vinicius Freitas <vinicius200396@hotmail.com>
2022-09-21 15:18:17 -0300

    first commit

---
+++ email/src/main/java/com/ms/email/repositories/EmailRepository.java
@@ -0,0 +1,10 @@
+package com.ms.email.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ms.email.models.EmailModel;
+
+public interface EmailRepository extends JpaRepository<EmailModel,Long> {
+    
+    
+}
